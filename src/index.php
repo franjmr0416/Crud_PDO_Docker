5c6480c57651127d48d54de8409a6be04493adb0
franjmr0416 <franjmr16@gmail.com>
2022-10-19 18:33:23 -0500

    add: bootstrap

--- src/index.php
+++ src/index.php
@@ -10,11 +10,20 @@ $db->conectar();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="public/css/bootstrap.min.css" />
+  <script src="public/js/bootstrap.bundle.min.js"></script>
+  <title>CRUD PDO</title>
 </head>
 
-<body>
-
+<body class="py-3">
+  <main class="container">
+    <div class="row">
+      <div class="col">
+        <h4>Productos</h4>
+        <a href="nuevo.php" class="btn btn-primary float-right">Nuevo</a>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
